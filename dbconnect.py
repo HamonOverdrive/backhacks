b432e5bdf66e124f08bd65de207bb07f75879c1a
Robin246 <robinleesuns13@gmail.com>
2017-11-06 21:42:36 -0800

    mysql connection returns dictionary hopefully part 2

--- dbconnect.py
+++ dbconnect.py
@@ -1,4 +1,5 @@
-import MySQLdb
+
+import MySQLdb.cursors
 # hint how to create python anywhere connection code with mysql https://www.pythonanywhere.com/forums/topic/11305/
 def connection():
     conn = MySQLdb.connect(host="robautomata.mysql.pythonanywhere-services.com",
