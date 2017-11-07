1d1e5be9de2167dbcacade250899748966e404da
Robin246 <robinleesuns13@gmail.com>
2017-11-06 16:50:54 -0800

    Register html working

--- backhack.py
+++ backhack.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template, flash, request, url_for, redirect, session
+from flask import Flask, render_template, flash, request, url_for, redirect, session, logging
+
+# not sure if i need below
+# from flask_mysqldb import MySQL
 
 #pycharms way of using FLask-WTForm
 from flask_wtf import Form
 from wtforms import StringField, BooleanField, validators, PasswordField
 from passlib.hash import sha256_crypt
-
-
 from functools import wraps
+
+#import mysql connection with this
+from dbconnect import connection
 
 app = Flask(__name__)
 
@@ -21,9 +25,23 @@ def homepage():
 def missionpage():
     return render_template("mission.html")
 
-@app.route('/login/')
-def login_page():
-    return render_template("login.html")
+# Register Form Class
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        return render_template('register.html')
+    return render_template('register.html', form=form)
 
 
 
---
+++ dbconnect.py
@@ -0,0 +1,11 @@
+import MySQLdb
+# hint how to create python anywhere connection code with mysql https://www.pythonanywhere.com/forums/topic/11305/
+def connection():
+    conn = MySQLdb.connect(host="robautomata.mysql.pythonanywhere-services.com",
+                           user = "robautomata",
+                           passwd = "niermankind6o",
+                           db = "robautomata$myflaskapp"
+                           )
+    c = conn.cursor()
+
+    return c, conn
