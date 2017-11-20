d17ae8a4be8352e07ab4274c3e1610a20dea5685
Robin246 <robinleesuns13@gmail.com>
2017-11-20 13:27:27 -0800

    separting form python file clean main py file test 2

--- backhack.py
+++ backhack.py
@@ -8,7 +8,7 @@ from flask import Flask, render_template, flash, request, url_for, redirect, ses
 # from wtforms import StringField, BooleanField, validators, PasswordField, TextAreaField
 from passlib.hash import sha256_crypt
 from functools import wraps
-from .forms import RegisterForm, CommentsForm, ArticleForm
+from forms import RegisterForm, CommentsForm, ArticleForm
 
 # import mysql connection with this
 from dbconnect import connection
