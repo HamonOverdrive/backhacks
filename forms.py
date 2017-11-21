ceaeaba1dc69f9adb381219a5277e0dc18b9b477
Robin246 <robinleesuns13@gmail.com>
2017-11-20 21:45:34 -0800

    Test 12 changing comment box again

--- forms.py
+++ forms.py
@@ -19,7 +19,7 @@ class RegisterForm(Form):
 
 # Comments Form Class
 class CommentsForm(Form):
-    comment = StringField('Comment', [validators.Length(min=0)])
+    comment = StringField('Comment', [validators.Length(min=1)])
 
 
 # Article/Discussion Form Class
